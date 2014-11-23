88dfac7d958c10f2a2e9d76836ab93d5538f048f
shanna wang <lashimmer@gmail.com>
2014-11-23 16:04:10 -0500

    store

--- laravel/app/views/store.php
+++ laravel/app/views/store.php
@@ -129,7 +129,7 @@
                         data: {username: 'ritalu', typeID: typeID , name: name},
                         success: function(data){
                             if (data = "You cannot afford this pet") {
-                                $('.buyform').append("<br><span style='color:red'>"+data+"</span>");
+                                $('.buyform').append("<br><span style='color:red';margin-top:10px>"+data+"</span>");
                             } else {
                                 console.log(data);
 
