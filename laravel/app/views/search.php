9aa000be5c23627ae095742cfcfaa1ced942bb11
shanna wang <lashimmer@gmail.com>
2014-11-25 09:25:45 -0500

    search

--- laravel/app/views/search.php
+++ laravel/app/views/search.php
@@ -17,7 +17,7 @@ if (Cookie::get('username') !== null) {
         <title>Settings</title>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
         <link href="./css/topbar.css" rel="stylesheet">
-        <link href="./css/inputs.css" rel="stylesheet">
+        <link href="./css/search.css" rel="stylesheet">
         <script src="./js/loadTopbar.js"></script>
 
     </head>
