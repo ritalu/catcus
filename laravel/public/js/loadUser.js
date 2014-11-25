a263aed021b3a64b7703a19e0d06918a042e8b31
shanna wang <lashimmer@gmail.com>
2014-11-25 10:35:39 -0500

    search

--- laravel/public/js/loadUser.js
+++ laravel/public/js/loadUser.js
@@ -3,26 +3,29 @@ var loadUser = function(username) {
     type:"GET",
     url:"../api/users/" + username,
     success: function(data){
-        if (data.length == 0) {
-          window.location = './error';
-        }
-        else {
+        
           renderUser(data);
-        }
+        
     }
   });
 }
 
 var renderUser = function (data) {
 
-  var level = Math.floor(Math.log(data.exp) / Math.log(2));
-  var totalExp = Math.pow(level + 1, 2);
-  
-  $('.panel').children('h1').html(data.username);
-  var bg = "url(" + data.picture + ") center center no-repeat white";
-  $('.profpic').css({"background": bg, "background-size":"cover"});
+  if (data.username == null) {
+    window.location = "./error";
+  }
+  else {
+    var level = Math.floor(Math.log(data.exp) / Math.log(2));
+    var totalExp = Math.pow(level + 1, 2);
 
-  $('.panel').children('h2').html("Level " + level);
+    $('.panel').children('h1').html(data.username);
+    var bg = "url(" + data.picture + ") center center no-repeat white";
+    $('.profpic').css({"background": bg, "background-size":"cover"});
+
+    $('.panel').children('h2').html("Level " + level);
+  }
+  
 
 }
 
